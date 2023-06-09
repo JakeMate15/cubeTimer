257dce89e010bc9e3ab96828a11c378837eaf342
Juan Angel Serrano Carreño <ghost_fire_heart@hotmail.com>
2023-06-08 19:22:04 -0600

    agregar tiempos

--- cuboRubic/js/Timer.js
+++ cuboRubic/js/Timer.js
@@ -1,9 +1,14 @@
-const startButton = document.getElementById('startButton');
-const stopButton = document.getElementById('stopButton');
 const timer = document.getElementById('timer');
+const timer1 = document.getElementById('timer1');
+const timer2 = document.getElementById('timer2');
+const timer3 = document.getElementById('timer3');
+const timer4 = document.getElementById('timer4');
+const timer5 = document.getElementById('timer5');
 
+let posicion = 0;
 let tiempo;
 let activo = false; 
+let tiempos = [];
 
 function startTimer() {
     let milisegundos = 0;
@@ -14,23 +19,27 @@ function startTimer() {
     }, 10); 
 
     activo = true;
-    startButton.disabled = true;
-    stopButton.disabled = false;
 }
 
 function detener() {
+    tiempos[posicion] = timer.textContent;
+    posicion++;
+    if (posicion > 4) {
+        posicion = 0;
+    }
     clearInterval(tiempo);
-
     activo = false;
-    startButton.disabled = false;
-    stopButton.disabled = true;
+    timer1.textContent = tiempos[0];
+    timer2.textContent = tiempos[1];
+    timer3.textContent = tiempos[2];
+    timer4.textContent = tiempos[3];
+    timer5.textContent = tiempos[4];
 }
 
 function formatTime(milisegundos) {
     const minutos = Math.floor((milisegundos % 3600000) / 60000).toString().padStart(2, '0');
     const segundos = Math.floor((milisegundos % 60000) / 1000).toString().padStart(2, '0');
     const ms = (milisegundos % 1000).toString().padStart(3, '0');
-
     return `${minutos}:${segundos}.${ms}`;
 }
 
@@ -43,6 +52,3 @@ document.addEventListener('keydown', (event) => {
         }
     }
 });
-
-startButton.addEventListener('click', startTimer);
-stopButton.addEventListener('click', stopTimer);
